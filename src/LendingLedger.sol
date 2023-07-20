a122c595ce679ddc176d029b5c0f4f6de99e48e7
Roman Böhringer <r.boehringer@opencore.ch>
2023-07-20 19:30:46 +0200

    Explicit checkpointing

--- src/LendingLedger.sol
+++ src/LendingLedger.sol
@@ -29,15 +29,15 @@ contract LendingLeder {
     ) private {
         uint256 currEpoch = (block.timestamp / WEEK) * WEEK;
         uint256 lastUserUpdateEpoch = lendingMarketBalancesEpoch[_market][_lender];
+        uint256 updateUntilEpoch = Math.min(currEpoch, _forwardTimestampLimit);
         if (lastUserUpdateEpoch > 0 && lastUserUpdateEpoch < currEpoch) {
             // Fill in potential gaps in the user balances history
             uint256 lastUserBalance = lendingMarketBalances[_market][_lender][lastUserUpdateEpoch];
-            uint256 updateUntilEpoch = Math.min(currEpoch, _forwardTimestampLimit);
             for (uint256 i = lastUserUpdateEpoch; i <= updateUntilEpoch; i += WEEK) {
                 lendingMarketBalances[_market][_lender][i] = lastUserBalance;
             }
-            lendingMarketBalancesEpoch[_market][_lender] = updateUntilEpoch;
         }
+        lendingMarketBalancesEpoch[_market][_lender] = updateUntilEpoch;
     }
 
     /// @notice Fill in gaps in the market total balances history (if any exist)
@@ -46,21 +46,42 @@ contract LendingLeder {
     function _checkpoint_market(address _market, uint256 _forwardTimestampLimit) private {
         uint256 currEpoch = (block.timestamp / WEEK) * WEEK;
         uint256 lastMarketUpdateEpoch = lendingMarketTotalBalanceEpoch[_market];
+        uint256 updateUntilEpoch = Math.min(currEpoch, _forwardTimestampLimit);
         if (lastMarketUpdateEpoch > 0 && lastMarketUpdateEpoch < currEpoch) {
             // Fill in potential gaps in the market total balances history
             uint256 lastMarketBalance = lendingMarketTotalBalance[_market][lastMarketUpdateEpoch];
-            uint256 updateUntilEpoch = Math.min(currEpoch, _forwardTimestampLimit);
             for (uint256 i = lastMarketUpdateEpoch; i <= updateUntilEpoch; i += WEEK) {
                 lendingMarketTotalBalance[_market][i] = lastMarketBalance;
             }
-            lendingMarketTotalBalanceEpoch[_market] = updateUntilEpoch;
         }
+        lendingMarketTotalBalanceEpoch[_market] = updateUntilEpoch;
+    }
+
+    /// @notice Trigger a checkpoint explicitly.
+    ///    Never needs to be called explicitly, but could be used to ensure the checkpoints within the other functions consume less gas (because they need to forward less epochs)
+    /// @param _market Address of the market
+    /// @param _forwardTimestampLimit Until which epoch (provided as timestamp) should the update be applied. If it is higher than the current epoch timestamp, this will be used.
+    function checkpoint_market(address _market, uint256 _forwardTimestampLimit) external {
+        require(lendingMarketTotalBalanceEpoch[_market] > 0, "No deposits for this market");
+        _checkpoint_market(_market, _forwardTimestampLimit);
+    }
+
+    /// @param _market Address of the market
+    /// @param _lender Address of the lender
+    /// @param _forwardTimestampLimit Until which epoch (provided as timestamp) should the update be applied. If it is higher than the current epoch timestamp, this will be used.
+    function checkpoint_lender(
+        address _market,
+        address _lender,
+        uint256 _forwardTimestampLimit
+    ) external {
+        require(lendingMarketBalancesEpoch[_market][_lender] > 0, "No deposits for this lender in this market");
+        _checkpoint_lender(_market, _lender, _forwardTimestampLimit);
     }
 
     /// @notice Function that is called by the lending market on cNOTE deposits / withdrawals
     /// @param _lender The address of the lender
     /// @param _delta The amount of cNote deposited (positive) or withdrawn (negative)
-    function syncLedger(address _lender, int256 _delta) external {
+    function sync_ledger(address _lender, int256 _delta) external {
         address lendingMarket = msg.sender; // TODO: Validate
 
         _checkpoint_lender(lendingMarket, _lender, type(uint256).max);
