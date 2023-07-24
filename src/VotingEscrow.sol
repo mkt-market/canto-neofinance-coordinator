318de2ce7777a84353e53a6d6cccf2ea54600579
Roman Böhringer <r.boehringer@opencore.ch>
2023-07-24 21:27:11 +0200

    Remove unnecessary import

--- src/VotingEscrow.sol
+++ src/VotingEscrow.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.16;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @title  VotingEscrow
 /// @author Curve Finance (MIT) - original concept and implementation in Vyper
