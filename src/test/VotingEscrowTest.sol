67442d8d5ff85776a387974c7a697774593d65f1
rotcivegaf <victorfage@gmail.com>
2023-07-26 22:13:32 +0200

    add test

---
+++ src/test/VotingEscrowTest.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {DSTest} from "ds-test/test.sol";
+import {Utilities} from "./utils/Utilities.sol";
+import {console} from "./utils/Console.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+import {VotingEscrow} from "../VotingEscrow.sol";
+
+contract VotingEscrowTest is DSTest {
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+
+    VotingEscrow votingEscrow;
+
+    function setUp() public {
+        votingEscrow = new VotingEscrow("Test", "TEST");
+    }
+
+    function testTryCreateLockWithZeroValue() public {
+        vm.expectRevert("Only non zero amount");
+        votingEscrow.createLock(0);
+    }
+}
