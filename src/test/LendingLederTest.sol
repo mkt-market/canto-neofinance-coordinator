ff5af17ef433c4d6b641f93a6035e3b0420811fc
rotcivegaf <victorfage@gmail.com>
2023-07-27 01:13:18 +0200

    fix

--- src/test/LendingLederTest.sol
+++ src/test/LendingLederTest.sol
@@ -23,7 +23,7 @@ contract LendingLederTest is DSTest {
         lendingLeder = new LendingLeder(address(0), address(this));
     }
 
-    function testTryClaimForUserThatNeverDeposited() public {
+    function testTryClaimForEpochWithoutSetRewards() public {
         address payable alice = users[0];
         vm.label(alice, "Alice");
         address market = address(6);
@@ -49,59 +49,3 @@ contract LendingLederTest is DSTest {
         );
     }
 }
-
-/*
-pragma solidity >=0.8.0;
-
-import {DSTest} from "ds-test/test.sol";
-import {Utilities} from "./utils/Utilities.sol";
-import {console} from "./utils/Console.sol";
-import {Vm} from "forge-std/Vm.sol";
-
-import {LendingLeder} from "../LendingLedger.sol";
-
-contract LendingLederTest is DSTest {
-    Vm internal immutable vm = Vm(HEVM_ADDRESS);
-
-    LendingLeder lendingLeder;
-
-    function setUp() public {
-        lendingLeder = new LendingLeder(address(0), address(0));
-    }
-
-    function testTryClaimWithInvalidStartEpoch() public {
-        uint256 someWeeks = 7 * lendingLeder.WEEK();
-
-        vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(
-            address(0),
-            someWeeks + 1,
-            type(uint256).max
-        );
-
-        vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(
-            address(0),
-            someWeeks - 1,
-            type(uint256).max
-        );
-    }
-
-    function testTryClaimWithInvalidEndEpoch() public {
-        uint256 someWeeks = 7 * lendingLeder.WEEK();
-
-        vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(
-            address(0),
-            type(uint256).max,
-            someWeeks + 1
-        );
-
-        vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(
-            address(0),
-            type(uint256).max,
-            someWeeks - 1
-        );
-    }
-}*/
