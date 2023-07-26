fc4fdd121cbafac04966398470fa0de4f52bde4d
gzeon <im@gzeon.dev>
2023-07-26 21:21:29 +0100

    test: createLock basic functionality #4

---
+++ src/test/VotingEscrow.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.16;
+
+import "forge-std/Test.sol";
+import "../VotingEscrow.sol";
+
+contract VotingEscrowTest is Test {
+    VotingEscrow public ve;
+
+    address public constant user1 = address(10001);
+
+    function setUp() public {
+        ve = new VotingEscrow("Voting Escrow", "VE");
+        vm.deal(user1, 1 ether);
+    }
+
+    uint256 public constant WEEK = 7 days;
+
+    function _floorToWeek(uint256 _t) internal pure returns (uint256) {
+        return (_t / WEEK) * WEEK;
+    }
+
+    function testSuccessCreateLock() public {
+        // Lock with a duration 5 year should be created with delegated set to msg.sender
+        vm.prank(user1);
+        ve.createLock{value: 100}(100);
+        assertEq(
+            ve.lockEnd(user1),
+            _floorToWeek(block.timestamp + ve.LOCKTIME())
+        );
+        (, , , address delegatee) = ve.locked(user1);
+        assertEq(delegatee, user1);
+    }
+}
