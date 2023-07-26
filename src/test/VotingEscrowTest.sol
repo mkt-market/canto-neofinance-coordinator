d41aa28c236b16d171cd02e679a88c7a6b37e669
rotcivegaf <victorfage@gmail.com>
2023-07-26 22:50:29 +0200

    Add test #11

---
+++ src/test/VotingEscrowTest.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {DSTest} from "ds-test/test.sol";
+import {Utilities} from "./utils/Utilities.sol";
+import {console} from "./utils/Console.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+import {VotingEscrow} from "../VotingEscrow.sol";
+
+contract VotingEscrowTest is DSTest {
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+
+    VotingEscrow votingEscrow;
+
+    function setUp() public {
+        votingEscrow = new VotingEscrow("Test", "TEST");
+    }
+
+    function testTryWithdrawForNonExpiredLock() public {
+        votingEscrow.createLock{ value: 1 }(1);
+
+        vm.expectRevert("Lock not expired");
+        votingEscrow.withdraw();
+    }
+}
