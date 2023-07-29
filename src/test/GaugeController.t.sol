6623b568a064a288e210266021c406dc98d797bc
franfran <fgg67@hotmail.fr>
2023-07-29 18:43:32 +0300

    reverts when multi vote > 10000

--- src/test/GaugeController.t.sol
+++ src/test/GaugeController.t.sol
@@ -26,13 +26,7 @@ contract GaugeControllerTest is DSTest, StdAssertions {
     function setUp() public {
         utils = new Utilities();
         users = utils.createUsers(5);
-        (gov, user1, user2, gague1, gague2) = (
-            users[0],
-            users[1],
-            users[2],
-            users[3],
-            users[4]
-        );
+        (gov, user1, user2, gague1, gague2) = (users[0], users[1], users[2], users[3], users[4]);
 
         ve = new VotingEscrow("VotingEscrow", "VE");
         gc = new GaugeController(address(ve), address(gov));
@@ -105,6 +99,17 @@ contract GaugeControllerTest is DSTest, StdAssertions {
         gc.vote_for_gauge_weights(user2, 999999);
     }
 
+    function testMultiVoteWithInvalidWeight() public {
+        vm.startPrank(gov);
+        gc.add_gauge(gague1);
+        gc.add_gauge(gague2);
+
+        ve.createLock{value: 1 ether}(1 ether);
+        gc.vote_for_gauge_weights(gague1, 5000);
+        vm.expectRevert("Used too much power");
+        gc.vote_for_gauge_weights(gague2, 5001);
+    }
+
     function testVoteLockExpiresTooSoon() public {
         vm.prank(gov);
         gc.add_gauge(user1);
@@ -146,11 +151,7 @@ contract GaugeControllerTest is DSTest, StdAssertions {
         gc.vote_for_gauge_weights(user2, 900);
 
         // check
-        assertApproxEqRel(
-            gc.get_gauge_weight(user1) * 10,
-            gc.get_total_weight(),
-            0.00001e18
-        );
+        assertApproxEqRel(gc.get_gauge_weight(user1) * 10, gc.get_total_weight(), 0.00001e18);
 
         vm.stopPrank();
     }
@@ -170,9 +171,6 @@ contract GaugeControllerTest is DSTest, StdAssertions {
         gc.vote_for_gauge_weights(gague1, 5000); // vote 50% for gague1
         gc.vote_for_gauge_weights(gague2, 5000); // vote 50% for gauge2
 
-        assertEq(
-            (gc.get_total_weight() * 5000) / 10000,
-            gc.get_gauge_weight(gague1)
-        );
+        assertEq((gc.get_total_weight() * 5000) / 10000, gc.get_gauge_weight(gague1));
     }
 }
