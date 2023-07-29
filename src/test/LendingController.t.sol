714f56ff53a9c397db9bcf9741edf144b0532456
ctf-sec <jeffcx416@gmail.com>
2023-07-29 09:14:06 -0400

    add test 38

---
+++ src/test/LendingController.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {DSTest} from "ds-test/test.sol";
+import {Utilities} from "./utils/Utilities.sol";
+import {console} from "./utils/Console.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+import "../LendingLedger.sol";
+
+contract DummyGaugeController {
+    function gauge_relative_weight_write(
+        address _gauge,
+        uint256 _time
+    ) external returns (uint256) {
+        return 1 ether;
+    }
+}
+
+contract LendingLedgerTest is DSTest {
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+
+    Utilities internal utils;
+    address payable[] internal users;
+
+    LendingLeder ledger;
+    DummyGaugeController controller;
+    address goverance;
+
+    uint256 public constant WEEK = 7 days;
+
+    function setUp() public {
+        utils = new Utilities();
+
+        users = utils.createUsers(5);
+
+        goverance = users[0];
+
+        controller = new DummyGaugeController();
+
+        ledger = new LendingLeder(address(controller), goverance);
+    }
+
+    function testAddWhitelistLendingMarket() public {
+        address lendingMarket = vm.addr(5201314);
+
+        vm.prank(goverance);
+        ledger.whiteListLendingMarket(lendingMarket, true);
+
+        bool isWhitelisted = ledger.lendingMarketWhitelist(lendingMarket);
+        assertTrue(isWhitelisted);
+    }
+}
