0d711da6e0b095082608a50f63d10c8fe7da43f3
Roman Böhringer <r.boehringer@opencore.ch>
2023-08-03 11:59:25 +0200

    lendingLeder -> lendingLedger

--- src/test/LendingLedger.t.sol
+++ src/test/LendingLedger.t.sol
@@ -8,38 +8,38 @@ import {Vm} from "forge-std/Vm.sol";
 
 import {LendingLedger} from "../LendingLedger.sol";
 
-contract LendingLederTest is DSTest {
+contract LendingLedgerTest is DSTest {
     Vm internal immutable vm = Vm(HEVM_ADDRESS);
 
     Utilities internal utils;
     address payable[] internal users;
 
-    LendingLedger lendingLeder;
+    LendingLedger lendingLedger;
 
     function setUp() public {
         utils = new Utilities();
         users = utils.createUsers(5);
-        lendingLeder = new LendingLedger(address(0), address(0));
+        lendingLedger = new LendingLedger(address(0), address(0));
     }
 
     function testTryClaimWithInvalidStartEpoch() public {
-        uint256 someWeeks = 7 * lendingLeder.WEEK();
+        uint256 someWeeks = 7 * lendingLedger.WEEK();
 
         vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(address(0), someWeeks + 1, type(uint256).max);
+        lendingLedger.claim(address(0), someWeeks + 1, type(uint256).max);
 
         vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(address(0), someWeeks - 1, type(uint256).max);
+        lendingLedger.claim(address(0), someWeeks - 1, type(uint256).max);
     }
 
     function testTryClaimWithInvalidEndEpoch() public {
-        uint256 someWeeks = 7 * lendingLeder.WEEK();
+        uint256 someWeeks = 7 * lendingLedger.WEEK();
 
         vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(address(0), type(uint256).max, someWeeks + 1);
+        lendingLedger.claim(address(0), type(uint256).max, someWeeks + 1);
 
         vm.expectRevert("Invalid timestamp");
-        lendingLeder.claim(address(0), type(uint256).max, someWeeks - 1);
+        lendingLedger.claim(address(0), type(uint256).max, someWeeks - 1);
     }
 
     function testTryClaimForEpochWithoutSetRewards() public {
@@ -48,19 +48,19 @@ contract LendingLederTest is DSTest {
         address market = address(6);
         vm.label(market, "market");
 
-        lendingLeder.whiteListLendingMarket(market, true);
+        lendingLedger.whiteListLendingMarket(market, true);
 
-        uint256 WEEK = lendingLeder.WEEK();
+        uint256 WEEK = lendingLedger.WEEK();
 
         vm.warp(block.timestamp + WEEK);
 
         vm.prank(market);
-        lendingLeder.sync_ledger(alice, 1);
+        lendingLedger.sync_ledger(alice, 1);
 
         vm.warp(block.timestamp + WEEK);
 
         vm.expectRevert("Reward not set yet");
         vm.prank(alice);
-        lendingLeder.claim(market, (block.timestamp % WEEK) * WEEK, WEEK);
+        lendingLedger.claim(market, (block.timestamp % WEEK) * WEEK, WEEK);
     }
 }
