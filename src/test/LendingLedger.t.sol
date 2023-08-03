e124920905e77b46f6bcf820821702da65d22472
Roman Böhringer <r.boehringer@opencore.ch>
2023-08-03 11:51:09 +0200

    Name

--- src/test/LendingLedger.t.sol
+++ src/test/LendingLedger.t.sol
@@ -6,15 +6,15 @@ import {Utilities} from "./utils/Utilities.sol";
 import {console} from "./utils/Console.sol";
 import {Vm} from "forge-std/Vm.sol";
 
-import {LendingLeder} from "../LendingLedger.sol";
+import {LendingLedger} from "../LendingLedger.sol";
 
 contract LendingLederTest is DSTest {
     Vm internal immutable vm = Vm(HEVM_ADDRESS);
 
-    LendingLeder lendingLeder;
+    LendingLedger lendingLeder;
 
     function setUp() public {
-        lendingLeder = new LendingLeder(address(0), address(0));
+        lendingLeder = new LendingLedger(address(0), address(0));
     }
 
     function testTryClaimWithInvalidStartEpoch() public {
