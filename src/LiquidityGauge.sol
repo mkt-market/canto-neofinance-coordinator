8a655def3348ba2f0d9b289e08e7e5d19b075e60
akshatcx <akshat.c2k@gmail.com>
2024-03-16 05:29:23 +0530

    fix comments

--- src/LiquidityGauge.sol
+++ src/LiquidityGauge.sol
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.16;
 
-import {VotingEscrow} from "./VotingEscrow.sol";
-import {GaugeController} from "./GaugeController.sol";
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 interface LendingLedger {
     function sync_ledger(address _lender, int256 _delta) external;
@@ -29,7 +25,7 @@ contract LiquidityGauge is ERC20, ERC20Burnable {
         lendingLedger = _lendingLedger;
     }
 
-    /// @notice Function called by user to deposit market tokens
+    /// @notice Function called by user to deposit underlying tokens
     /// @param _amount The amount of token to be deposited
     function depositUnderlying(uint256 _amount) external {
         address _user = msg.sender;
@@ -38,7 +34,7 @@ contract LiquidityGauge is ERC20, ERC20Burnable {
         _mint(_user, _amount);
     }
 
-    /// @notice Function called by the user to withdraw market tokens
+    /// @notice Function called by the user to withdraw underlying tokens
     /// @param _amount The amount of token to be withdrawn
     function withdrawUnderlying(uint256 _amount) external {
         address _user = msg.sender;
